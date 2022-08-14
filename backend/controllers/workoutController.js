259caf7ff6fc09cd6b38447e1645d7c407943972
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-08-14 16:45:39 +0200

    #6, #7 controllers koniec

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,110 @@
+// importowanie modelu bo będziemy go tutaj używać
+const Workout = require("../models/workoutModel");
+
+// importowanie biblioteki, zeby zwalidować id z urla z rządzania klienta
+const mongoose = require("mongoose");
+
+//get all workouts
+const getWorkouts = async (req, res) => {
+  // pusty obiket jako argumnet funckji .find - zwróci wszystkie obiekty z bazy
+  //   obiekty są posortowane malejące wzgledem daty utworzenia dokumentu w bazie - najnowsze jako pierwsze
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  // parametry z urla tutaj /api/workouts/:id są w obiekcie zadania w property .params
+  const { id } = req.params;
+
+  //   zanim poszukamy obiektu, to musimy upewnoić sie że id z urla jest prawidłowego typu jaki wymaga mongoDB type of ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // bez tego jest wewnętrzny bład z mongoDB
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  // suzkanie pojedynczegodokuemntu
+  const workout = await Workout.findById(id);
+  if (!workout) {
+    // jeśli nie istneije w bazie
+    // trzeba użyć 'return' bo inaczej odpali resztę kodu a myu chcemy od razy zwrotkę jak nie znajdzie
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  //   zwraca obiekt, bo nie jest nullem
+  res.status(200).json(workout);
+};
+
+// create new workout
+const createWorkout = async (req, res) => {
+  // dobieranie się do ciała, jakie jest wysłane do serwera dzięki middleware app.use(express.json());
+  const { title, load, reps } = req.body;
+  // tworzenie nowego dokumentu w kolekcji
+
+  // add doc to db
+  try {
+    // Workout.create() jest asynchroniczne, więc trzeba zmienić sygnaturę funkcji createWorkout
+    const workout = await Workout.create({ title, load, reps });
+    // zwrotka z serwera - stauts i nowo utworzony obiekt
+    res.status(200).json(workout);
+  } catch (error) {
+    // obiekt error ma atrybut message i to jego wyświetlamy.zwracamy klientowi
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params; //id z urla
+
+  //sprawdzanie czy id jest w prawidłowym formacie
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // bez tego jest wewnętrzny bład z mongoDB
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  // staramy się usunąć
+  // UWAGA w mongoDB id nazwya sie z podkreślinkiem z proszu `_id`
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    // jeśli nie istneije w bazie taki obiekt
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  //   jak obiekt byl w bazie i go usunęliśmy to zwracamy klientowi właśnie usunięty obiket
+  res.status(200).json(workout);
+};
+
+// update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params; //id z urla
+
+  //sprawdzanie czy id jest w prawidłowym formacie
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // bez tego jest wewnętrzny bład z mongoDB
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  //   próba update - dwa argumnetu 1: probuje znaleźc obiekt po paramaterach, 2: reprezentuje update któy chcemy zrobić
+  //   ...res.body spread opertor robiumy aktulaizje z body któy wysąll nam klient - nie trzeba osobno destrukturyzacji robić z request.body
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!workout) {
+    // jeśli nie istneije w bazie taki obiekt
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  // zwracam obiekt PRZED aktualizacją
+  res.status(200).json(workout);
+};
+
+// eksportowanie funkcji
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,46 +1,30 @@
+// ten plik ma służyć tylko do rejestracji routerów, dlatego funckje tworzące sa gdzie ingdziej żeby ten plik nie spuchł
 const express = require("express");
-// importowanie modelu
-const Workout = require("../models/workoutModel");
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // attach the habdler to this router
-// GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "Get all workouts" });
-});
+// GET all workouts - rejestacja funckji z kontrollera - bez wywołania - brak nawiasow ()
+router.get("/", getWorkouts);
 
-// GET signle workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "Get a single workout" });
-});
+// GET signle workout rejestacja funckji z kontrollera - bez wywołania - brak nawiasow ()
+router.get("/:id", getWorkout);
 
-// POST a new workout - zmiana sygnatury na async (req, res) - mozna teraz używać await w środku
-router.post("/", async (req, res) => {
-  // dobieranie się do ciała, jakie jest wysłane do serwera dzięki middleware app.use(express.json());
-  const { title, load, reps } = req.body;
-  // tworzenie nowego dokumentu w kolekcji
-
-  //   try catch block
-  try {
-    // Workout.create() jest asynchroniczne, więc trzeba zmienić sygnaturę router.post
-    const workout = await Workout.create({ title, load, reps });
-    // zwrotka z serwera - stauts i nowo utworzony obiekt
-    res.status(200).json(workout);
-  } catch (error) {
-    // obiekt error ma atrybut message i to jego wyświetlamy.zwracamy klientowi
-    res.status(400).json({ error: error.message });
-  }
-});
+//POST - create a new Workoput document
+router.post("/", createWorkout);
 
 // DELETE a  workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a  workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a workout" });
-});
+router.patch("/:id", updateWorkout);
 
+// export modułu
 module.exports = router;
